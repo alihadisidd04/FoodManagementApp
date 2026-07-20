29cfb0ea903ae263588b46023e15654ed773b04b
alihadisidd04 <alihadi.sidd04@gmail.com>
2026-07-20 13:59:49 +0500

    added modal for checkout and also add close button functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,16 +1,22 @@
 import { useContext } from "react";
 import CartContext from "../store/CartContext.jsx";
-import currencyFormatter from "../util/formatting.js";
+import { currencyFormatter } from "../util/formatting.js";
 import Button from "./UI/Button.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
+import Modal from "./UI/Modal.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0,
   );
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
   return (
-    <Modal>
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
@@ -21,7 +27,9 @@ export default function Cart() {
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly>Close</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
         <Button>Go to Checkout</Button>
       </p>
     </Modal>
