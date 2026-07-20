c2373fdeb1379016a84fc45e5a6f35f2f6d86e67
alihadisidd04 <alihadi.sidd04@gmail.com>
2026-07-20 17:05:30 +0500

    Added error handling

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,18 +1,19 @@
-import { useState, useEffect } from "react";
+import useHttp from "./hooks/useHttp";
 import MealItem from "./MealItem";
+import Error from "./Error";
+const requestConfig = {};
 export default function Meals() {
-  const [loadMeals, setLoadMeals] = useState([]);
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-      if (!response.ok) {
-        //...
-      }
-      const meals = await response.json();
-      setLoadMeals(meals);
-    }
-    fetchMeals();
-  }, []);
+  const {
+    data: loadMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
+  if (isLoading) {
+    return <p className="center">Fetching meals...</p>;
+  }
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error} />;
+  }
 
   return (
     <ul id="meals">
